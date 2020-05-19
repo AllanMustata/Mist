7c3064807f33b626c7bdd5410f803309c70b5c9d
marky1337 <daniel.ionascu99@gmail.com>
2020-05-20 02:53:53 +0300

    Update Game class

--- src/main/java/screen/Game.java
+++ src/main/java/screen/Game.java
@@ -3,12 +3,39 @@ package screen;
 public class Game {
     private String title;
     private String genre;
+    private String devname;
     private double price;
+    private int numRatings;
+    private double averageRating;
 
-    public Game(String title, String genre, double price) {
+    public Game()
+    {
+
+    }
+
+    public Game(String title, String genre, String devname, double price, int numRatings, double averageRating) {
         this.title = title;
         this.genre=genre;
+        this.devname = devname;
         this.price=price;
+        this.numRatings = numRatings;
+        this.averageRating = averageRating;
+    }
+
+    public int getNumRatings() {
+        return numRatings;
+    }
+
+    public void setNumRatings(int numRatings) {
+        this.numRatings = numRatings;
+    }
+
+    public double getAverageRating() {
+        return averageRating;
+    }
+
+    public void setAverageRating(double averageRating) {
+        this.averageRating = averageRating;
     }
 
     public String getTitle() {
@@ -17,6 +44,10 @@ public class Game {
 
     public String getGenre() {
         return genre;
+    }
+
+    public String getDevname() {
+        return devname;
     }
 
     public double getPrice() {
@@ -29,6 +60,10 @@ public class Game {
 
     public void setGenre(String genre) {
         this.genre = genre;
+    }
+
+    public void setDevname(String devname) {
+        this.devname = devname;
     }
 
     public void setPrice(double price) {
