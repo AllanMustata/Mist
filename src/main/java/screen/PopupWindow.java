6d597a426a2dcc320255febe0291284a79bbbf68
marky1337 <daniel.ionascu99@gmail.com>
2020-05-20 02:53:38 +0300

    PopupWindow: Create 2 more overloaded 'Display' methods

--- src/main/java/screen/PopupWindow.java
+++ src/main/java/screen/PopupWindow.java
@@ -11,6 +11,26 @@ import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class PopupWindow {
+    public static void Display(String title, String description){
+        Stage window=new Stage();
+
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle(title);
+
+        Label label=new Label(description);
+        Button okbutton=new Button("OK");
+        okbutton.setOnAction(e->window.close());
+
+        VBox layout=new VBox(10);
+        layout.getChildren().addAll(label, okbutton);
+        layout.setAlignment(Pos.CENTER);
+
+        Scene scene=new Scene(layout, 300, 150);
+        window.setScene(scene);
+        window.showAndWait();
+
+    }
+
     public static void Display(String title, String description, String buttonText){
         Stage window=new Stage();
 
@@ -29,5 +49,24 @@ public class PopupWindow {
         window.setScene(scene);
         window.showAndWait();
 
+    }
+
+    public static void Display(String title, String description, String buttonText, int x, int y){
+        Stage window=new Stage();
+
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle(title);
+
+        Label label=new Label(description);
+        Button okbutton=new Button(buttonText);
+        okbutton.setOnAction(e->window.close());
+
+        VBox layout=new VBox(10);
+        layout.getChildren().addAll(label, okbutton);
+        layout.setAlignment(Pos.CENTER);
+
+        Scene scene=new Scene(layout, x, y);
+        window.setScene(scene);
+        window.showAndWait();
     }
 }
