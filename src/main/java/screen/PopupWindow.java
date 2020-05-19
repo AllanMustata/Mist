499305137898804d990b244930b0b4c5aabc7f71
marky1337 <daniel.ionascu99@gmail.com>
2020-05-19 17:45:38 +0300

    PopupWindow: Add button text parameter. Move to 'screen' packet.

--- src/main/java/screen/PopupWindow.java
+++ src/main/java/screen/PopupWindow.java
@@ -1,3 +1,5 @@
+package screen;
+
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -9,14 +11,14 @@ import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class PopupWindow {
-    public static void Display(String title, String description){
+    public static void Display(String title, String description, String buttonText){
         Stage window=new Stage();
 
         window.initModality(Modality.APPLICATION_MODAL);
         window.setTitle(title);
 
         Label label=new Label(description);
-        Button okbutton=new Button("OK");
+        Button okbutton=new Button(buttonText);
         okbutton.setOnAction(e->window.close());
 
         VBox layout=new VBox(10);
